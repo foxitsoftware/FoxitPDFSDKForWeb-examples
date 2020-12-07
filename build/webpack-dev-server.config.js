138888900dd5164a22f2f7c335c0a0e5f42ebb70
y1j2x34 <y1j2x34@qq.com>
2020-12-07 15:17:29 +0800

    chore: open page after start

--- build/webpack-dev-server.config.js
+++ build/webpack-dev-server.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 module.exports = {
     contentBase: path.resolve(__dirname, '../dist'),
+    open: true,
+    openPage: 'examples/UIExtension/complete_webViewer/index.html',
     port: 9999,
     inline: true,
-    host: '0.0.0.0',
     disableHostCheck: true,
     clientLogLevel: 'error',
     proxy: {
